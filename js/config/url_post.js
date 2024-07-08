fdabc28610fb38a7afb4ecbfb8b4aff690948a04
gryzlegrizz <gryzlegrizz@gmail.com>
2024-07-08 19:11:47 +0700

    chore: Update urlPOST to use the correct Heroku URL

--- js/config/url_post.js
+++ js/config/url_post.js
@@ -1,4 +1,4 @@
-export let urlPOST = "URLHEROKU/insert"
+export let urlPOST = "https://tb-parfume2024-34a7b650de40.herokuapp.com/insert"
 
 export function AmbilResponse(result) {
     console.log(result); //menampilkan response API pada console
