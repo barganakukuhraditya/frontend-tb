420cd89e6d60d4b32c62d41d32ea2eae1b2f5dfa
gryzlegrizz <gryzlegrizz@gmail.com>
2024-07-08 15:52:24 +0700

    feat: Add product page and fetch data from API

---
+++ js/temp/table.js
@@ -0,0 +1,29 @@
+export let isiTabel=
+`
+    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            #MERK#
+        </th>
+        <td class="px-6 py-4">
+            #NAMAPARFUM#
+        </td>
+        <td class="px-6 py-4">
+            #JENIS#
+        </td>
+        <td class="px-6 py-4">
+            #UKURAN#
+        </td>
+        <td class="px-6 py-4">
+            #DESKRIPSI#
+        </td>
+        <td class="px-6 py-4">
+            #STOK#
+        </td>
+        <td class="px-6 py-4">
+            #HARGA#
+        </td>
+        <td class="px-6 py-4">
+            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+        </td>
+    </tr>
+`
